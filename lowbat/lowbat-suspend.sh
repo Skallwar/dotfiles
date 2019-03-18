e32a3015219afccb4d20a014d38364d1b763529d
ESTBLC <estblcsk@gmail.com>
2019-03-18 23:04:22 +0000

    Add suspend on low battery script

---
+++ lowbat/lowbat-suspend.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+acpi -b | awk -F'[,:%]' '{print $2, $3}' | {
+	read -r status capacity
+
+	if [ "$status" = Discharging -a "$capacity" -lt 5 ]; then
+		logger "Critical battery threshold"
+		systemctl suspend
+    else
+        echo "Battty level = $capacity"
+	fi
+}
