7743039046da938c46f849aae7f8a8b07f058c01
Skallwar <estblcsk@gmail.com>
2019-08-30 14:48:50 +0200

    Add lock and suspend keybind

--- config/dwm/config.h
+++ config/dwm/config.h
@@ -50,6 +50,10 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 };
 
+/* Custom function def */
+static void lock(const Arg *arg);
+static void suspend(const Arg *arg);
+
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
@@ -69,15 +73,15 @@ static const char *termcmd[]  = { "alacritty", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,	                XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,	                    XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY|ShiftMask,		XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY|ShiftMask,	        	XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|ShiftMask,                       XK_Return, zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
@@ -100,6 +104,8 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY|ControlMask,           XK_l,      lock,           {0} },
+	{ MODKEY|ControlMask,           XK_s,      suspend,          {0} },
 	{ MODKEY|ShiftMask,             XK_e,      quit,           {0} },
 };
 
@@ -120,3 +126,12 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
 
+static void lock(const Arg *arg)
+{
+    system("~/.dotfiles/lock.sh");
+}
+
+static void suspend(const Arg *arg)
+{
+    system("systemctl suspend");
+}
