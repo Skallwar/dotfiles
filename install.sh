652df61a4235f9eb0399993908231ea403d97ef9
Esteban Blanc <estblcsk@gmail.com>
2020-07-24 18:03:12 +0200

    misc: Switch to stow

--- install.sh
+++ install.sh
@@ -1,62 +1,52 @@
 #!/bin/sh
 
-path_origin="$(pwd)"
-path_target="$1"
+arch=$(cat /etc/os-release | grep "^ID" | cut -d "=" -f2)
+shells="bash zsh"
 
-if [ -z "$path_target" ]
-then
-    echo "Usage: ./install.sh [TARGET_DIRECTORY]"
-    exit 1
-elif [[ $path_target != /* ]]
-then
-    path_target=${path_target%/*}
-    path_target="$path_origin/$path_target"
-fi
+echo Welcome $USER@$HOSTNAME on $arch !
 
-echo -n "Install dotfiles? [y/N]: "
+echo "Install dependencies"
+yay -S 'stow'
+
+echo -n "Wich shell do you use ?: "
+read shell
+yay -S "$shell"
+
+echo -n "Install dotfiles? [y/N]: ";
 read needdot
-if [ "$needdot" = "y" -o "$needdot" = "Y" ]
-then
-echo Installing config files
-ln -sfv $path_origin/config/vimrc $path_target/.vimrc
-ln -sfv $path_origin/config/zshrc $path_target/.zshrc
-ln -sfv $path_origin/config/xinitrc $path_target/.xinitrc
-ln -sfv $path_origin/config/makepkg.conf /etc/makepkg.conf
-echo
-fi
-
-echo -n "Install i3 config? [y/N]: "
-read needi3
-if [ "$needi3" = "y" -o "$needi3" = "Y" ]
-then
-    ln -sfv $path_origin/config/i3/i3config $path_target/.config/i3/config
+if [ "$needdot" = "y" -o "$needdot" = "Y" ]; then
+    for folder in $(ls -d */); do
+        stow "$folder"
+    done
 fi
 
 echo -n "Install laptop services? [y/N]: "
-read needServices
-if [ "$needServices" = "y" -o "$needServices" = "Y" ]
-then
+read needLaptopServices
+if [ "$needLaptopServices" = "y" -o "$needLaptopServices" = "Y" ]; then
     #Lowbat-suspend
-    sudo cp -fv $path_origin/services/lowbat-suspend.timer /etc/systemd/system &&
-    sudo cp -fv $path_origin/services/lowbat-suspend.service /etc/systemd/system &&
-    sudo cp -fv $path_origin/services/lowbat-suspend.sh /etc/systemd/system&&
-    sudo systemctl enable lowbat-suspend.timer && 
-    sudo systemctl start lowbat-suspend.timer
+    systemctl --user enable lowbat-suspend.timer && 
+    systemctl --user start lowbat-suspend.timer
 
     #Suspend
-    sudo cp -fv $path_origin/services/suspend.service /etc/systemd/system &&
-    sudo cp -fv $path_origin/services/suspend.sh /etc/systemd/system &&
-    sudo systemctl enable suspend.service
+    systemctl --user enable suspend.service
 fi
 
 echo -n "Install U2F config? [y/N]: "
 read needU2F
-if [ "$needU2F" = "y" -o "$needU2F" = "Y" ]
-then
-    sudo pacman -S pam-u2f
+if [ "$needU2F" = "y" -o "$needU2F" = "Y" ]; then
+    yay -S pam-u2f
     sudo sed -i '2iauth		sufficient	pam_u2f.so origin=pam://hostname appid=pam://hostname cue [prompt=Please touch the device]' /etc/pam.d/sudo
     sudo sed -i '3iauth		sufficient	pam_u2f.so origin=pam://hostname appid=pam://hostname' /etc/pam.d/login
     sudo sed -i '6iauth		sufficient	pam_u2f.so origin=pam://hostname appid=pam://hostname' /etc/pam.d/i3lock
+fi
+
+echo -n "Install spotifyd ? [y/N]: "
+read needSpotifyd
+if [ "$needSpotifyd" = "y" -o "$needSpotifyd" = "Y" ]; then
+    yay spotifyd spotify-tui
+
+    systemctl --user enable spotifyd.service &&
+    systemctl --user start spotifyd.service
 fi
 
 exit 0
