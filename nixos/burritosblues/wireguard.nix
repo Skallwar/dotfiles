2f4a78f95acc477ccd3b3c468321c452264f4646
Esteban Blanc <estblcsk@gmail.com>
2024-05-13 16:19:47 +0200

    nixos: burritosblues: wireguard nm configs in sops
    Signed-off-by: Esteban Blanc <estblcsk@gmail.com>

---
+++ nixos/burritosblues/wireguard.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, lib, ... }:
+
+{
+  networking.firewall = {
+    allowedTCPPorts = [ 53 ];
+    allowedUDPPorts = [ 53 30566 ]; # Clients and peers can use the same port, see listenport
+    checkReversePath = false;
+  };
+
+  sops.secrets = {
+    paris_config = {
+      sopsFile = ./wireguard_secret.yaml;
+      owner = "root";
+      path = "/etc/NetworkManager/system-connections/paris.nmconnection";
+      restartUnits = ["NetworkManager.service"];
+    };
+    antibes_config = {
+      sopsFile = ./wireguard_secret.yaml;
+      owner = "root";
+      path = "/etc/NetworkManager/system-connections/antibes.nmconnection";
+      restartUnits = ["NetworkManager.service"];
+    };
+    antibes-lan_config = {
+      sopsFile = ./wireguard_secret.yaml;
+      owner = "root";
+      path = "/etc/NetworkManager/system-connections/antibes-lan.nmconnection";
+      restartUnits = ["NetworkManager.service"];
+    };
+  };
+}
