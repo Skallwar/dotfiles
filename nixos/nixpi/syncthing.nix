fec2f8682876368f5437f4b02036a22cf4388ef7
Esteban Blanc <estblcsk@gmail.com>
2024-09-16 11:51:49 +0000

    nixos: syncthing: add phone backup and playnite
    Signed-off-by: Esteban Blanc <estblcsk@gmail.com>

--- nixos/nixpi/syncthing.nix
+++ nixos/nixpi/syncthing.nix
@@ -14,6 +14,7 @@
         "redmi" = { id = "BQ4RQPP-B7GVSPM-QUKRF55-PFWWLMV-FYFA7BF-RIQOESL-GL5ULRA-S3BPRA4"; };
         "burritosblues" = { id = "FT4XMTE-CMU4LN2-WVFJCML-Q2PLCMP-YYRRJDJ-2QZR53M-JYD34SL-I55DLA7"; };
         "PCMASTERACE" = { id = "XNISIE3-PXBKVLA-TSD47VH-FNHIOFN-XYRDRZJ-B7ST2EC-NAVQ4CT-D6YINAT"; };
+        "Macha-PC" = { id = "KEH534Q-26GUJVO-5IUPCM3-WV6R33K-BS6UQ7M-S4DZPRJ-TUKIRKX-CR24FQO"; };
       };
 
       folders = {
@@ -25,6 +26,15 @@
         "Admin" = {
           path = "/data/syncthing/Admin";
           devices = [ "redmi" "burritosblues" "PCMASTERACE" ];
+        };
+        "Playnite" = {
+          path = "/data/syncthing/Playnite";
+          devices = [ "PCMASTERACE" "Macha-PC" ];
+        };
+        "PhoneBackups" = {
+          id = "sig36-5ap6f";
+          path = "/data/syncthing/PhoneBackups";
+          devices = [ "redmi" "PCMASTERACE" ];
         };
       };
     };
