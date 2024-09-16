ca297f86be0c0b6ddac0befdd7b282c88193abf2
Esteban Blanc <estblcsk@gmail.com>
2024-09-16 14:46:24 +0200

    nixos: cli: use unstable nvim
    Signed-off-by: Esteban Blanc <estblcsk@gmail.com>

--- nixos/cli.nix
+++ nixos/cli.nix
@@ -3,7 +3,7 @@
 {
   environment.systemPackages = with pkgs; [
     # CLI
-    neovim helix htop killall tree man-pages ncdu tmux ripgrep fd
+    unstable.neovim helix htop killall tree man-pages ncdu tmux ripgrep fd
     bash starship
     nix-output-monitor
   ];
