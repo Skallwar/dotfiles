15477d4f15b7d6acae85cb978077f05331b5cbde
Esteban Blanc <estblcsk@gmail.com>
2025-10-23 16:52:54 +0200

    nixos: nixpi: enable mDNS over wirguard
    I can now ping nixpi.local from burritosblues when connected via
    wireguard
    
    Signed-off-by: Esteban Blanc <estblcsk@gmail.com>

--- nixos/base.nix
+++ nixos/base.nix
@@ -9,6 +9,8 @@
   services.avahi = {
     enable = true;
     nssmdns4 = true;
+    openFirewall = true;
+    allowPointToPoint = true;
   };
 
   # Never authorized password ssh login
--- nixos/nixpi/wireguard_server.nix
+++ nixos/nixpi/wireguard_server.nix
@@ -29,6 +29,7 @@
           # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
           postSetup = ''
             ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 192.168.27.0/24 -o end0 -j MASQUERADE
+            ${pkgs.iproute2}/bin/ip link set dev wg0 multicast on
           '';
 
           # This undoes the above command
@@ -50,16 +51,31 @@
               # Public key of the peer (not a file path).
               publicKey = "8ttBKJ8oUe9hf8+sXHzDKmh3vf5Vx2GccDX+CvRoU2U=";
               # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
-              allowedIPs = [ "192.168.27.2/32" ];
+              allowedIPs = [ "192.168.27.2/32" "224.0.0.251/32" ];
             }
             {
               # Redmi Note 10 T
               publicKey = "MK8S6a0BpWsAY/8SZzLW1sikluJyMNVJcCj+PP9I4EY=";
-              allowedIPs = [ "192.168.27.3/32" ];
+              allowedIPs = [ "192.168.27.3/32" "224.0.0.251/32" ];
             }
           ];
         };
       };
+    };
+  };
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+    allowPointToPoint = true;
+    reflector = true;
+    publish = {
+      enable = true;
+      domain = true;
+      addresses = true;
+      hinfo = true;
+      workstation = true;
     };
   };
 }
