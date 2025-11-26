e2b4f6c1616350882cc02aa83cd8b8277dd699f2
Esteban Blanc <estblcsk@gmail.com>
2025-11-26 11:48:51 +0100

    nvim: add options for gitcommit filetype
    Signed-off-by: Esteban Blanc <estblcsk@gmail.com>

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -4,3 +4,18 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+local function set_language_config()
+    local filetype = vim.bo.filetype
+
+    if filetype == "gitcommit" then
+        vim.wo.colorcolumn = '72'
+    end
+end
+
+vim.api.nvim_create_autocmd({"FileType"}, {
+  pattern = {"*"},
+  callback = function()
+    set_language_config()
+  end
+})
