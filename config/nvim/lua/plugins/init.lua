862e2b3ea0d31f5a7bdef838d3e0bff468d683a8
Esteban Blanc <estblcsk@gmail.com>
2026-07-27 11:02:39 +0200

    nvim: Add dap config
    Signed-off-by: Esteban Blanc <estblcsk@gmail.com>

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -36,4 +36,18 @@ return {
     "tpope/vim-fugitive",
     cmd = { "G", "Git" },
   },
+
+  {
+    "mfussenegger/nvim-dap",
+    event = "VeryLazy",
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+        "nvim-neotest/nvim-nio",
+        "jay-babu/mason-nvim-dap.nvim",
+        "theHamsta/nvim-dap-virtual-text",
+    },
+    config = function()
+      require "configs.dap-config"
+    end,
+  },
 }
